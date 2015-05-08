c75044acf340fce8155b9188a2fb0b0fa50d089d
boolean93 <boolean93@163.com>
2015-05-08 12:13:14 +0800

    add

---
+++ date_iOS/IndexViewController.m
@@ -0,0 +1,37 @@
+//
+//  IndexViewController.m
+//  date_iOS
+//
+//  Created by BooleanMac on 15/5/7.
+//  Copyright (c) 2015年 Boolean93. All rights reserved.
+//
+
+#import "IndexViewController.h"
+
+@interface IndexViewController ()
+
+@end
+
+@implementation IndexViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
---
+++ date_iOS/NavigationController.h
@@ -0,0 +1,13 @@
+//
+//  NavigationController.h
+//  date_iOS
+//
+//  Created by BooleanMac on 15/5/7.
+//  Copyright (c) 2015年 Boolean93. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NavigationController : UINavigationController
+
+@end
